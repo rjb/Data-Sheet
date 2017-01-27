79c2199ea0930e09a20d3a601b7d63ca08d50931
Raffy Banks <raffybanks@gmail.com>
2017-01-26 21:04:04 -0800

    Added optional params

--- lib/google_sheet/spreadsheet.rb
+++ lib/google_sheet/spreadsheet.rb
@@ -1,14 +1,26 @@
 module GoogleSheet
   class Spreadsheet
     attr_reader :values
+    attr_reader :google_spreadsheet
 
     def initialize(connection, id)
       @id = id
       @connection = connection
+      @google_spreadsheet = @connection.get_spreadsheet(@id)
     end
 
-    def values(range = 'A:Z')
-      @values ||= @connection.get_spreadsheet_values(@id, range).values
+    def values(options = {})
+      range = options[:range] || 'A:Z'
+
+      params = {
+        fields: options[:fields] || nil,
+        quota_user: options[:quota_user] || nil,
+        major_dimension: options[:major_dimension] || nil,
+        value_render_option: options[:value_render_option] || nil,
+        date_time_render_option: options[:date_time_render_option] || nil,
+      }
+
+      @values ||= @connection.get_spreadsheet_values(@id, range, params).values
     end
   end
 end
