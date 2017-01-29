bca7761aaa0d969bfc0a76091e1a2e6c45a6a718
Raffy Banks <raffybanks@gmail.com>
2017-01-29 09:16:07 -0800

    Created private methods to load cell values and set sheet properties

--- lib/google_sheet/sheet.rb
+++ lib/google_sheet/sheet.rb
@@ -4,33 +4,35 @@ module GoogleSheet
   class Sheet
     UPDATABLE_PROPERTIES = %w(title index).freeze
 
-    attr_writer :title
-    attr_writer :index
+    attr_reader :id
+    attr_accessor :title
+    attr_accessor :index
 
     def initialize(connection, spreadsheet, api_results)
       @connection = connection
       @spreadsheet = spreadsheet
       @api_results = api_results
+      set_properties
     end
 
     def values(opts = {})
-      @values ||= @connection.get_spreadsheet_values(@spreadsheet.id, "#{title}!A:Z", opts).values
-    end
-
-    def id
-      @id ||= @api_results.properties.sheet_id
-    end
-
-    def title
-      @title ||= @api_results.properties.title
-    end
-
-    def index
-      @index ||= @api_results.properties.index
+      @values ||= load_values(opts)
     end
 
     def save
       UpdateRequest::Sheet.new(@connection, @spreadsheet, self).update!
+    end
+
+    private
+
+    def load_values(opts = {})
+      @connection.get_spreadsheet_values(@spreadsheet.id, "#{title}!A:Z", opts).values
+    end
+
+    def set_properties
+      @id = @api_results.properties.sheet_id
+      @title = @api_results.properties.title
+      @index = @api_results.properties.index
     end
   end
 end
