07dea13c23a0d1914450e63fd62f89f27f8991ec
Raffy Banks <raffybanks@gmail.com>
2017-01-29 08:46:51 -0800

    Dropped overlapping and confusing #values method

--- lib/google_sheet/spreadsheet.rb
+++ lib/google_sheet/spreadsheet.rb
@@ -11,20 +11,6 @@ module GoogleSheet
       @connection = connection
     end
 
-    def values(options = {})
-      range = options[:range] || 'A:Z'
-
-      params = {
-        fields: options[:fields] || nil,
-        quota_user: options[:quota_user] || nil,
-        major_dimension: options[:major_dimension] || nil,
-        value_render_option: options[:value_render_option] || nil,
-        date_time_render_option: options[:date_time_render_option] || nil,
-      }
-
-      @connection.get_spreadsheet_values(@id, range, params).values
-    end
-
     def sheets
       @sheets ||= @connection.get_spreadsheet(id).sheets.map do |api_sheet|
         Sheet.new(@connection, self, api_sheet)
