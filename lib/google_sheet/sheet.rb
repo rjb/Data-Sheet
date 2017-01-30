a686ab65f77148ee668735cf0c2f1d2eddde7637
Raffy Banks <raffybanks@gmail.com>
2017-01-29 20:43:47 -0800

    Added properties to params and added #load_values

--- lib/google_sheet/sheet.rb
+++ lib/google_sheet/sheet.rb
@@ -4,15 +4,16 @@ module GoogleSheet
   class Sheet
     UPDATABLE_PROPERTIES = %w(title index).freeze
 
-    attr_reader :id
+    attr_reader :spreadsheet_id
     attr_accessor :title
     attr_accessor :index
 
-    def initialize(connection, spreadsheet, api_results)
-      @connection = connection
+    def initialize(service, spreadsheet, title, index)
+      @service = service
       @spreadsheet = spreadsheet
-      @api_results = api_results
-      set_properties
+      @spreadsheet_id = spreadsheet.id
+      @title = title
+      @index = index
     end
 
     def values(opts = {})
@@ -20,19 +21,13 @@ module GoogleSheet
     end
 
     def save
-      UpdateRequest::Sheet.new(@connection, @spreadsheet, self).update!
+      UpdateRequest::Sheet.new(@service, @spreadsheet, self).update!
     end
 
     private
 
     def load_values(opts = {})
-      @connection.get_spreadsheet_values(@spreadsheet.id, "#{title}!A:Z", opts).values
-    end
-
-    def set_properties
-      @id = @api_results.properties.sheet_id
-      @title = @api_results.properties.title
-      @index = @api_results.properties.index
+      @service.get(:sheet_values, self, opts)
     end
   end
 end
