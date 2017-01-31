6c36f951695786c210e5efca2ff7354f96b862e7
Raffy Banks <raffybanks@gmail.com>
2017-01-30 23:10:52 -0800

    Dropped unnecessary instance variable, spreadsheet_id, in favor of spreadsheet.id

--- lib/google_sheet/sheet.rb
+++ lib/google_sheet/sheet.rb
@@ -3,14 +3,13 @@ module GoogleSheet
     UPDATABLE_PROPERTIES = %w(title index).freeze
 
     attr_reader :id
-    attr_reader :spreadsheet_id
+    attr_reader :spreadsheet
     attr_accessor :title
     attr_accessor :index
 
     def initialize(service, spreadsheet, id, title, index)
       @service = service
       @spreadsheet = spreadsheet
-      @spreadsheet_id = spreadsheet.id
       @id = id
       @title = title
       @index = index
